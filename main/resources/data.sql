1947e4cf678ba43b1c617ca25af3e798dc4c6f8f
Aquat1c <49980958+Aquat1c@users.noreply.github.com>
2021-12-09 21:51:50 +0200

    Delete main/resources directory

--- main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into film (name, minutes, week)
-values ('Titanic ', 100, 1),
-       ('Jurassic Park', 120, 2),
-       ('Spider Man', 210, 3);
-
-insert into ticket_type (name, multiplier)
-values ('17:00', 1.0),
-       ('18:00', 1.5),
-       ('19:00', 2.0);
